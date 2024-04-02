a157d6f9c10f5fb37c4118336452066107263eda
Lil-Kr <cxqybox@163.com>
2024-04-02 09:44:27 +0800

    change user api structure

--- system/back-end/src/main/java/com/cy/single/blog/api/sys/UserApi.java
+++ system/back-end/src/main/java/com/cy/single/blog/api/sys/UserApi.java
@@ -1,7 +1,12 @@
 package com.cy.single.blog.api.sys;
 
+import com.cy.single.blog.aspect.annotations.CheckAuth;
+import com.cy.single.blog.aspect.annotations.RecordLogger;
 import com.cy.single.blog.base.ApiResp;
+import com.cy.single.blog.common.holder.RequestHolder;
 import com.cy.single.blog.pojo.entity.sys.SysUser;
+import com.cy.single.blog.pojo.req.user.UserLoginAdminReq;
+import com.cy.single.blog.pojo.req.user.UserRegisterReq;
 import com.cy.single.blog.pojo.req.user.UserSaveReq;
 import com.cy.single.blog.service.SysUserService;
 import lombok.extern.slf4j.Slf4j;
@@ -22,6 +27,26 @@ public class UserApi {
 
     @Autowired
     private SysUserService userService;
+
+    @RecordLogger
+    @PutMapping("/login")
+    public ApiResp<String> login(@RequestBody @Valid UserLoginAdminReq req) {
+        return userService.adminLogin(req);
+    }
+
+    @DeleteMapping("/logout")
+    @CheckAuth
+    public ApiResp<Integer> logout() {
+        // 移除用户
+        RequestHolder.remove();
+        return ApiResp.success("用户已退出");
+    }
+
+    @PostMapping("/register")
+    @CheckAuth
+    public ApiResp<Integer> register(@RequestBody @Valid UserRegisterReq req) {
+        return userService.registerAdmin(req);
+    }
 
     @GetMapping("/getUserById/{id}")
     public ApiResp<SysUser> getUserById(@PathVariable("id") Long id) {
